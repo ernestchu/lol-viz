a99290c742d4bbeb125b0103e4b2b399bab3aff4
ernestchu <b073040018@nsysu.edu.tw>
2022-05-17 22:46:42 +0800

    add travis

--- src/views/DataView.vue
+++ src/views/DataView.vue
@@ -1,11 +1,33 @@
 <script setup>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { ref } from 'vue'
 import JsonViewer from 'vue-json-viewer'
 
+const fileNotFound = ref(false)
+const data = ref()
+const dataURI = ref(useRoute().params.dataURI)
+const dataURL = new URL(`../assets/sampleData/${dataURI.value}.json`, import.meta.url).href
+fetch(dataURL)
+  .then(res => res.json())
+  .then(json => data.value = json)
+  .catch(() => fileNotFound.value = true)
+
+const router = useRouter()
+function reloadWithDataURI () {
+  window.location.href = `/data/${dataURI.value}` 
+}
 </script>
 
 <template>
-  MatchId: {{ useRoute().params.dataURI }}
+  <h2>{{ dataURI }}.json</h2>
+  {{ dataURL }}
+  <template v-if="fileNotFound">
+    <h3>File not found, please try another file name:</h3>
+    <form @submit.prevent="reloadWithDataURI">
+      <input type="text" v-model="dataURI">
+      <button>Try again</button>
+    </form>
+  </template>
 
-  <!-- <JsonViewer :value="timeline" /> -->
+  <JsonViewer v-if="data" :value="data" />
 </template>
