d1b38fe0bf73f78284f809a94f2f5d34e0065987
ernestchu <ernestchu@users.noreply.github.com>
2022-06-08 17:17:37 +0000

    deploy: f469e5beedfedd9cf3a2591f409830b1806f8fc3

--- assets/ThreeTestView.ca2b1df7.js
+++ assets/ThreeTestView.ca2b1df7.js
@@ -1,4 +1,4 @@
-import{as as dc,ay as fc,W as pc}from"./index.5e3956e4.js";/**
+import{as as dc,ay as fc,W as pc}from"./index.2b941df2.js";/**
  * @license
  * Copyright 2010-2022 Three.js Authors
  * SPDX-License-Identifier: MIT
