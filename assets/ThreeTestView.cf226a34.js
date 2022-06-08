6c2a876e60c895df9ee31cc8c8aae77bc0c849e5
ernestchu <ernestchu@users.noreply.github.com>
2022-06-08 21:55:04 +0000

    deploy: 609eb662fa01b0f4557742cc99f5a8606e6d7650

--- assets/ThreeTestView.cf226a34.js
+++ assets/ThreeTestView.cf226a34.js
@@ -1,4 +1,4 @@
-import{as as dc,ay as fc,W as pc}from"./index.cd4876fb.js";/**
+import{as as dc,ay as fc,W as pc}from"./index.63b35b26.js";/**
  * @license
  * Copyright 2010-2022 Three.js Authors
  * SPDX-License-Identifier: MIT
